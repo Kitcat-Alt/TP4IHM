7c1f193c165bf1743363f860f6fba219478476a5
Kitcat-Alt <streetfion65@gmail.com>
2025-06-12 10:59:37 +0200

    ajout de la javadoc

--- exec.sh
+++ exec.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 clear
+javadoc -d doc/ --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls src/*java
 javac -d bin/ --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls src/*java
 java -cp bin/:img/ --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls Pendu
